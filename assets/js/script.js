e9a9fca3207fa332e471380855765a69fd18d953
devbharath2k <devbharath21k@gmail.com>
2023-11-08 12:52:57 +0530

    'new'

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+AOS.init();
+
+$(document).ready(function() {
+    resizeFullHeightSection();
+
+    // Handle window resize events to update the section height
+    $(window).resize(function() {
+        resizeFullHeightSection();
+    });
+});
+
+function resizeFullHeightSection() {
+    var windowHeight = $('.scroll section').height();
+    $(".scroll-next").css("margin-top", `calc( 100vh + ${windowHeight/2 +'px'} )`);
+}
+
+var mbnew = $('.card-img-overlay').height();
+
+$('.hero-banner').css("margin-bottom", mbnew ,'+px')
