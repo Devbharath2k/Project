c0b23d1f3208004fa6a201f524d2722062278cb6
Sundar <sundar10072001@gmail.com>
2023-11-08 18:08:31 +0530

    sundar changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,23 @@ function resizeFullHeightSection() {
     var windowHeight = $('.scroll section').height();
     $(".scroll-next").css("margin-top", `calc( 100vh + ${windowHeight/2 +'px'} )`);
 }
+$(document).ready(function() {
+    var rocketContainer = $("#rocket-container");
+    var rocket = $("#rocket").height();
 
-var mbnew = $('.card-img-overlay').height();
+    $(window).scroll(function() {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var websiteHeight = $("body").height();
+
+        var percentage = (scrollPos / (websiteHeight - windowHeight)) * 100;
+
+        rocketContainer.css("bottom", `calc(${percentage}%)`);
+        rocketContainer.css("transform", `translate(0, ${percentage}%)`);
+    });
+
+});
+
+var mbnew = $('.card-img-overlay').height()+100;
 
 $('.hero-banner').css("margin-bottom", mbnew ,'+px')
